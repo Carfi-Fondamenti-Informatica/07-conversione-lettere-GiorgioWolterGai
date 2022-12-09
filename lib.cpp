5a89d77022f2cd4f4e1136e32327a834f455a3c2
GiorgioWolterGai <118194394+GiorgioWolterGai@users.noreply.github.com>
2022-12-09 11:41:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+int main(){
+    char n=0; cin >> n;
+    if (funzione==true) {
+        cout << n;
+    } else if (funzione==false) {
+      cout << "errore";
+    }
+    return 0;
+}
